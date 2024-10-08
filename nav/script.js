27a9f78b9fab3b0804d654d82342bb0449fe0562
𝐀𝐌𝐀𝐍 𝐏𝐀𝐍𝐃𝐄𝐘. <69503838+aman-ap-official@users.noreply.github.com>
2024-10-08 12:13:26 +0530

    Responsive Nav bar

---
+++ nav/script.js
@@ -0,0 +1,12 @@
+document.addEventListener("DOMContentLoaded", function () {
+    const animationPage = document.getElementById('animation-page');
+    const contactForm = document.getElementById('contact-form');
+    const menuToggle = document.querySelector('.menu-toggle');
+  
+    // // Toggle the menu when the button is clicked
+    // menuToggle.addEventListener('click', () => {
+    //     menuToggle.classList.toggle('active');
+    // });
+  
+    // });
+  
